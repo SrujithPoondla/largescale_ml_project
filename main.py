bdf1302118a04c9952655d010104a7d0a7024acf
Srujith Poondla <spoondla@wisc.edu>
2018-03-26 15:46:43 -0500

    Modified the main and train to take arguments to decide whether to run redis or plain hogwild

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import argparse
 import timeit
-
+import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,10 +11,12 @@ import torch.multiprocessing as mp
 
 
 from common_functions import push_params_redis, get_shape, push_params_memcache
-from train import train
+from train_redis import train_redis
+from train_plain import train_plain
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--disable_redis', dest='is_redis', action='store_false')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
@@ -29,11 +31,11 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
-parser.add_argument('--num-processes', type=int, default=3, metavar='N',
+parser.add_argument('--num-processes', type=int, default=1, metavar='N',
                     help='how many training processes to use (default: 2)')
 
 
-class Net(nn.Module):
+class Net(nn.Module,):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
@@ -52,34 +54,46 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 if __name__ == '__main__':
+
+    # common code
     start_time = timeit.default_timer()
     args = parser.parse_args()
     torch.manual_seed(args.seed)
     model = Net()
-    # db = redis(db=0)
 
-    # db = client.Client(('localhost', 11211))
+    print("Number of proesses:"+ str(args.num_processes))
+    if args.is_redis:
+        print("Redis")
+    else:
+        print("Plain HogWild")
+    if args.is_redis:
+        db = redis(db=0)
+        # db = client.Client(('localhost', 11211))
+        # push params to redis cache
+        push_params_redis(model, db)
+        shapes = get_shape(model)
+        processes = []
+        for rank in range(args.num_processes):
+            p = mp.Process(target=train_redis, args=(rank, args, model, shapes, db))
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
+    else:
+        # push params to memcache
+        # push_params_memcache(model, db)
+        # shapes = get_shape(model)
+        # train(args, model, shapes, db)
 
-    # push params to redis cache
-    # push_params_redis(model, db)
-    # shapes = get_shape(model)
-    # train(args, model,shapes, db)
+        model.share_memory() # gradients are allocated lazily, so they are not shared here
 
-    #push params to memcache
-    # push_params_memcache(model, db)
-    # shapes = get_shape(model)
-    # train(args, model, shapes, db)
+        processes = []
+        for rank in range(args.num_processes):
+            p = mp.Process(target=train_plain, args=(rank, args, model))
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
 
-    model.share_memory() # gradients are allocated lazily, so they are not shared here
-
-    processes = []
-
-    for rank in range(args.num_processes):
-        p = mp.Process(target=train, args=(rank, args, model))
-        p.start()
-        processes.append(p)
-    for p in processes:
-        p.join()
-
-    # your code
-    print("Total execution time: {}".format(timeit.default_timer() - start_time))
+    # # your code
+    print("Total execution time: {}\n".format(timeit.default_timer() - start_time))
